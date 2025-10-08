380603a54a159a8ca9490ce73fe3e542f7b52879
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-10-08 14:45:19 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,24 @@
+
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maxDistance = function (words) {
+    let front = 0;
+    let back = words.length - 1;
+    let maxDistanceBetweenUnequalWords = 0;
+
+    while (front <= back) {
+        if (words[front] !== (words[words.length - 1])) {
+            maxDistanceBetweenUnequalWords = words.length - front;
+            break;
+        }
+        if (words[back] !== (words[0])) {
+            maxDistanceBetweenUnequalWords = back + 1;
+            break;
+        }
+        ++front;
+        --back;
+    }
+    return maxDistanceBetweenUnequalWords;
+};
